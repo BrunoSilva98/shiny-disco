dd6275c2888d34c1d2c372972b0ffaec87e83dc7
Bruno Passos da Silva <brunops98@hotmail.com>
2020-07-23 23:20:59 -0300

    creation of setPixel function

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -5,44 +5,50 @@
 #include "BitMapFileHeader.h"
 
 namespace bmp {
-    Bitmap::Bitmap(int width, int height): m_width(width), m_height(height),
-                m_pixels(new uint8_t[width * height * 3]{})
+
+Bitmap::Bitmap(int width, int height): m_width(width), m_height(height),
+            m_pixels(new uint8_t[width * height * 3]{})
+{
+}
+
+Bitmap::~Bitmap(){}
+
+bool Bitmap::write(std::string filename) 
+{
+    BitMapFileHeader fileHeader;
+    BitMapInfoHeader infoHeader;
+    auto headerSizes = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader);
+    auto totalPixels = m_height * m_width * 3;
+
+    fileHeader.fileSize = headerSizes + totalPixels;
+    fileHeader.dataOffset = headerSizes;
+
+    infoHeader.width = m_width;
+    infoHeader.height = m_height;
+
+    std::ofstream file;
+    file.open(filename.c_str(), std::ios::out|std::ios::binary);
+
+    if (!file)
     {
-    }
-
-    Bitmap::~Bitmap(){}
-    
-    bool Bitmap::write(std::string filename) 
-    {
-        BitMapFileHeader fileHeader;
-        BitMapInfoHeader infoHeader;
-        auto headerSizes = sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader);
-        auto totalPixels = m_height * m_width * 3;
-
-        fileHeader.fileSize = headerSizes + totalPixels;
-        fileHeader.dataOffset = headerSizes;
-
-        infoHeader.width = m_width;
-        infoHeader.height = m_height;
-
-        std::ofstream file;
-        file.open(filename.c_str(), std::ios::out|std::ios::binary);
-
-        if (!file)
-        {
-            return false;
-        }
-        
-        file.write((char *)&fileHeader, sizeof(fileHeader));
-        file.write((char *)&infoHeader, sizeof(infoHeader));
-        file.write((char *)m_pixels.get(), totalPixels);
-        file.close();
-        return true;
+        return false;
     }
     
-    void Bitmap::setPixel(int x, int y, std::uint8_t red, std::uint8_t green,
-                        std::uint8_t blue) 
-    {
-    }
+    file.write((char *)&fileHeader, sizeof(fileHeader));
+    file.write((char *)&infoHeader, sizeof(infoHeader));
+    file.write((char *)m_pixels.get(), totalPixels);
+    file.close();
+    return true;
+}
+
+void Bitmap::setPixel(int x, int y, std::uint8_t red, std::uint8_t green,
+                    std::uint8_t blue) 
+{
+    std::uint8_t *pixel = m_pixels.get();
+    pixel += ((y * 3) * m_width) + (x * 3);
+    pixel[0] = blue;
+    pixel[1] = green;
+    pixel[2] = red;
+}
     
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "Bitmap.h"
 
 int main(int, char**) {
-    bmp::Bitmap bitmap(800, 600);
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
+    bmp::Bitmap bitmap(WIDTH, HEIGHT);
+
+    for (size_t y = 0; y < HEIGHT; y++)
+    {
+        for (size_t x = 0; x < WIDTH; x++)
+        {
+            bitmap.setPixel(x, y, 255, 0, 0);
+        }
+        
+    }
+    
     bitmap.write("test.bmp");
     return 0;
 }

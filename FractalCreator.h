a660fa08b80691dc0b4d6f0ed82d32d477b555b2
Bruno Passos <brunops98@hotmail.com>
2020-08-13 10:29:40 -0300

    extracted methods from main and implemented in FractalCreator

--- FractalCreator.h
+++ FractalCreator.h
@@ -3,19 +3,32 @@
 
 #include <string>
 #include "Zoom.h"
+#include "Bitmap.h"
+#include "ZoomList.h"
 
 namespace bmp{
 
 class FractalCreator
 {
 public:
-    FractalCreator();
-    ~FractalCreator();
+    FractalCreator(int width, int height);
     void calculateIteration();
+    void calculateTotalIterations();
     void drawFractal();
-    void writeBitmap(std::string& name);
+    void writeBitmap(std::string name);
     void addZoom(const Zoom& zoom);
-    
+    int getWidth();
+    int getHeight();
+
+private:
+    int m_width;
+    int m_height;
+    int m_total;
+    std::unique_ptr<int[]> m_histogram;
+    std::unique_ptr<int[]> m_fractal;
+    Bitmap m_bitmap;
+    ZoomList m_zoomList;
+
 };
 }
 #endif // __FRACTALCREATOR_H__
--- main.cpp
+++ main.cpp
@@ -4,73 +4,17 @@
 #include <math.h>
 #include <utility>
 
-#include "Bitmap.h"
-#include "Mandelbrot.h"
-#include "Zoom.h"
-#include "ZoomList.h"
+#include "FractalCreator.h"
 
 int main(int, char**) {
-    const int WIDTH = 800;
-    const int HEIGHT = 600;
+    const int m_height = 600;
 
-    bmp::Bitmap bitmap(WIDTH, HEIGHT);
-    // double min = 999999;
-    // double max = -999999;
-
-    bmp::ZoomList zoomList(WIDTH, HEIGHT);
-    zoomList.add(bmp::Zoom(WIDTH/2, HEIGHT/2, 4.0/WIDTH));
-    zoomList.add(bmp::Zoom(295, HEIGHT - 202, 0.1));
-    zoomList.add(bmp::Zoom(312, HEIGHT - 304, 0.1));
-
-    std::unique_ptr<int[]> histogram(new int[bmp::Mandelbrot::MAX_ITERATIONS]{});
-    std::unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT]{});
-
-    for (int y = 0; y < HEIGHT; y++)
-    {
-        for (int x = 0; x < WIDTH; x++)
-        {
-            std::pair<double, double> coords = zoomList.doZoom(x, y);
-
-            int iterations = bmp::Mandelbrot::getIterations(coords.first, coords.second);
-            fractal[y*WIDTH+x] = iterations;
-
-            if (iterations != bmp::Mandelbrot::MAX_ITERATIONS)
-                histogram[iterations]++;
-        }   
-    }
-
-    int total = 0;
-    for(int i=0; i<bmp::Mandelbrot::MAX_ITERATIONS; i++)
-    {
-        total += histogram[i];
-    }
-
-    for (int y = 0; y < HEIGHT; y++)
-    {
-        for (int x = 0; x < WIDTH; x++)
-        {
-            std::uint8_t red = 0;
-            std::uint8_t green = 0;
-            std::uint8_t blue = 0;
-            int iterations = fractal[y * WIDTH + x];
-           
-            if (iterations != bmp::Mandelbrot::MAX_ITERATIONS)
-            {
-                double hue = 0.0;
-                for (int i = 0; i < iterations; i++)  
-                {
-                    hue += ((double)histogram[i]) / total;
-                }
-                
-                green = pow(255, hue);
-            }
-            bitmap.setPixel(x, y, red, green, blue);
-            
-        }
-        
-    }
-    
-
-    bitmap.write("test.bmp");
+    bmp::FractalCreator fractalCreator(800, 600);
+    fractalCreator.addZoom(bmp::Zoom(295, m_height - 202, 0.1));
+    fractalCreator.addZoom(bmp::Zoom(312, m_height - 304, 0.1));
+    fractalCreator.calculateIteration();
+    fractalCreator.calculateTotalIterations();
+    fractalCreator.drawFractal();
+    fractalCreator.writeBitmap("test.bmp");
     return 0;
 }
